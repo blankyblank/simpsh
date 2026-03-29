9d1e2de6464bf812b93a5af3c326cc7cf15a8a3c
blank <jwthaparc@gmail.com>
2026-03-29 03:43:32 -0500

    combined alias files and var files along with refactor changes

---
+++ env.c
@@ -0,0 +1,130 @@
+#include "simpsh.h"
+#include "env.h"
+#include "utils.h"
+
+alias *alias_tab[ALIAS_BUCKETS];
+shvar *var_tab[VAR_BUCKETS] = {NULL};
+static char *var_n(char *, size_t, size_t *);
+static char *varbrace_n(const char *, size_t, size_t *);
+void setvar(const char *, const char *);
+// int setvar(const char *, const char *, int);
+static char * lookupvar (char *);
+
+shvar *
+find_var(const char *name) {
+  shvar *v;
+  HASH_LOOKUP(var_tab, VAR_BUCKETS, name, name, shvar, v);
+  return v;
+}
+
+void
+setvar(const char *name, const char *val) {
+  HASH_INSERT(var_tab, VAR_BUCKETS, name, val, shvar, name, value);
+}
+
+void
+unset_var(const char *name) {
+  HASH_DELETE(var_tab, VAR_BUCKETS, name, shvar, name, value);
+}
+
+alias *
+find_alias(const char *name) {
+  alias *a;
+  HASH_LOOKUP(alias_tab, ALIAS_BUCKETS, name, name, alias, a);
+  return a;
+}
+
+void
+set_alias(const char *name, const char *val) {
+  HASH_INSERT(alias_tab, ALIAS_BUCKETS, name, val, alias, name, value);
+}
+
+void
+rm_alias(const char *name) {
+  HASH_DELETE(alias_tab, ALIAS_BUCKETS, name, alias, name, value);
+}
+char *
+var_n(char *args, size_t i, size_t *end) {
+  char *env_var, *vt, *var;
+  size_t j, vt_l;
+
+  for (j = i + 1;; j++) {
+    if ((isalnum(args[j]) == 0 && args[j] != '_') || args[j] == '\0') {
+      vt_l = j - (i + 1);
+
+      if (vt_l == 0) {
+        env_var = &args[i];
+        break;
+      } else {
+        vt = strndup(&args[i + 1], vt_l);
+        env_var = lookupvar(vt);
+        free(vt);
+        break;
+      }
+    }
+  }
+
+  *end = j;
+  var = strdup(env_var);
+  return var;
+}
+
+char *
+varbrace_n(const char *args, size_t i, size_t *end) {
+  char *vt, *var, *env_var;
+  size_t j, vt_l;
+
+  if (strchr(args, '}') == NULL)
+    return NULL;
+
+  for (j = i + 1;; j++) {
+    if (args[j] == '}') {
+      vt_l = j - (i + 2);
+      if (vt_l == 0)
+        return NULL;
+      else {
+        vt = strndup(&args[i + 2], vt_l);
+        env_var = lookupvar(vt);
+        break;
+      }
+    }
+  }
+
+  *end = j;
+  var = strdup(env_var);
+  return var;
+}
+
+char * lookupvar (char * vt) {
+  char *var;
+  int n;
+
+  if (is_posparam(vt)) {
+    n = atoi(vt);
+    var = get_posparam(n);
+  } else {
+    var = getvar(vt);
+  }
+  free(vt);
+
+  return var;
+}
+
+char *
+exp_var(char *word, size_t s, size_t *e) {
+  if (word[s] != '$')
+    return NULL;
+
+  if (word[s + 1] == '$') {
+    *e = s + 2;
+    return var_pid();
+  } else if (word[s + 1] == '?') {
+    *e = s + 2;
+    return statusvar();
+  } else if (word[s + 1] == '{') {
+    return varbrace_n(word, s, e);
+  } else {
+    return var_n(word, s, e);
+  }
+}
+
---
+++ env.h
@@ -0,0 +1,77 @@
+#ifndef VAR_H
+#define VAR_H
+
+#include "ctype.h"
+
+typedef struct alias alias;
+struct alias {
+  char *name;
+  char *value;
+  alias *next;
+};
+typedef struct shvar shvar;
+struct shvar {
+  char *name;
+  char *value;
+  shvar *next;
+};
+
+#define VAR_BUCKETS 39
+#define ALIAS_BUCKETS 64
+#define MAX_ALIAS_DEPTH 10
+extern shvar *var_tab[VAR_BUCKETS];
+extern alias *alias_tab[ALIAS_BUCKETS];
+
+extern char *exp_var(char *, size_t, size_t *);
+extern alias *find_alias(const char *);
+extern void set_alias(const char *, const char *);
+extern void rm_alias(const char *);
+
+static inline char *
+getvar(const char *vt) {
+  char *var;
+  if ((var = getenv(vt)) == NULL)
+    var = "";
+  return var;
+}
+
+static inline char *
+statusvar(void) {
+  char *buf = malloc(12);
+  snprintf(buf, 12, "%d", lstatus);
+  return buf;
+}
+
+
+static inline char *
+var_pid(void) {
+  return strdup(sh_pid_s);
+}
+
+static inline char *
+get_posparam(int n) {
+  if (n == 0)
+    return sh_argv0 ? sh_argv0 : "";
+
+  if (n < 0 || n > sh_argc)
+    return "";
+  return sh_argv[n - 1] ? sh_argv[n - 1] : "";
+}
+
+static inline int
+is_posparam(const char *var) {
+  size_t i;
+  size_t var_l = strlen(var);
+
+  if (!var || !*var)
+    return 0;
+  for (i = 0; i < var_l; i++) {
+    if (!isdigit(var[i]))
+      return 0;
+  }
+  return 1;
+}
+
+// vim: set filetype=c:
+#endif /* VAR_H */
+
