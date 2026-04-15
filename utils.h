1a4d7b442367b7238cb4d16eb7799e9695961046
blankyblank <jwthaparc@gmail.com>
2026-04-15 00:40:16 -0500

    got rid of hash table macros

--- utils.h
+++ utils.h
@@ -1,3 +1,4 @@
+/* util.h - misc helper functions */
 #ifndef MALLOC_H
 #define MALLOC_H
 
@@ -7,6 +8,7 @@
 #include <stddef.h>
 /* malloc and free or other small inlined functions */
 
+/** brief find name and value from name=value pair */
 static inline void
 read_assn(const char *assn, char **name, char **value) {
   char *eq;
@@ -22,22 +24,30 @@ read_assn(const char *assn, char **name, char **value) {
   }
 }
 
-
-static inline size_t array_len(char **arr) {
+/** get length of char* array */
+static inline size_t
+array_len(char **arr) {
   size_t n = 0;
-  while (arr[n]) n++;
+  while (arr[n])
+    n++;
   return n;
 }
 
-static inline int is_ws(char c) {
+/** check if char is whitespace */
+static inline int
+is_ws(char c) {
   return c == ' ' || c == '\t' || c == '\n';
 }
 
-static inline const char *skip_ws(const char *s) {
-  while (is_ws(*s)) s++;
+/** skip whitespace */
+static inline const char *
+skip_ws(const char *s) {
+  while (is_ws(*s))
+    s++;
   return s;
 }
 
+/** reallocate buffer to bufsize */
 static inline char *
 s_realloc(char *buf, size_t *bufsize) {
   char *t;
@@ -51,6 +61,7 @@ s_realloc(char *buf, size_t *bufsize) {
   return t;
 }
 
+/** free char* array */
 static inline void
 freeptr(char **args) {
   if (args != NULL) {
@@ -63,6 +74,7 @@ freeptr(char **args) {
   }
 }
 
+/** hash string to put in hash table */
 static inline unsigned int
 hash(const char *s, unsigned int buckets) {
   unsigned int h = 0;
@@ -72,97 +84,6 @@ hash(const char *s, unsigned int buckets) {
   }
   return h % buckets;
 }
-
-/* ============== HASH TABLE MACROS ============== */
-/*
- * HASH_LOOKUP - Find an entry in a hash table
- * @table:      The hash table array
- * @buckets:    Number of buckets
- * @name:       Key to search for (const char *)
- * @key_field:  Name of key field in struct (e.g., name)
- * @type:       Struct type (e.g., alias)
- * @out:        Output variable to store result
- *
- * Usage:
- *   alias *result;
- *   HASH_LOOKUP(alias_tab, ALIAS_BUCKETS, name, name, alias, result);
- *   if (result) { ... }
- */
-
-#define HASH_LOOKUP(table, buckets, name, key_field, type, out) \
-  do { \
-    unsigned int _i = hash(name, buckets); \
-    type *_e = table[_i]; \
-    while (_e) { \
-      if (strcmp(_e->key_field, name) == 0) \
-        break; \
-      _e = _e->next; \
-    } \
-    out = _e; \
-  } while (0)
-
-/*
- * HASH_INSERT - Insert or update an entry in a hash table
- * @table:      The hash table array
- * @buckets:    Number of buckets
- * @name:       Key name (const char *)
- * @val:        Value string (const char *)
- * @type:       Struct type
- * @key_field:  Name of key field in struct
- * @val_field:  Name of value field in struct
- *
- * If entry exists: frees old value, sets new value
- * If entry doesn't exist: creates new node at bucket head
- *
- * Usage:
- *   HASH_INSERT(alias_tab, ALIAS_BUCKETS, name, value, alias, name, value);
- */
-#define HASH_INSERT(table, buckets, name, val, type, key_field, val_field) \
-  do { \
-    type *_e = NULL; \
-    HASH_LOOKUP(table, buckets, name, key_field, type, _e); \
-    if (_e) { \
-      free(_e->val_field); \
-      _e->val_field = strdup(val); \
-    } else { \
-      _e = malloc(sizeof(type)); \
-      _e->key_field = strdup(name); \
-      _e->val_field = strdup(val); \
-      unsigned int _i = hash(name, buckets); \
-      _e->next = table[_i]; \
-      table[_i] = _e; \
-    } \
-  } while (0)
-
-/*
- * HASH_DELETE - Remove an entry from a hash table
- * @table:      The hash table array
- * @buckets:    Number of buckets
- * @name:       Key to delete
- * @type:       Struct type
- * @key_field:  Name of key field in struct
- * @val_field:  Name of value field in struct
- *
- * Usage:
- *   HASH_DELETE(alias_tab, ALIAS_BUCKETS, name, alias, name, value);
- */
-#define HASH_DELETE(table, buckets, name, type, key_field, val_field) \
-  do { \
-    unsigned int _i = hash(name, buckets); \
-    type **_prev = &table[_i]; \
-    type *_e = table[_i]; \
-    while (_e) { \
-      if (strcmp(_e->key_field, name) == 0) { \
-        *(_prev) = _e->next; \
-        free(_e->key_field); \
-        free(_e->val_field); \
-        free(_e); \
-        break; \
-      } \
-      _prev = &_e->next; \
-      _e = _e->next; \
-    } \
-  } while (0)
 
 /* vim: set filetype=c: */
 #endif /* !MALLOC_H */
