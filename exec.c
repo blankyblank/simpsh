e67c06041f075a5a94a3aa5e95f705c484e5265c
blankyblank <jwthaparc@gmail.com>
2026-04-26 08:40:20 -0500

    finished adding in pipes (still needs more testing probably)

--- exec.c
+++ exec.c
@@ -5,6 +5,7 @@
 #include "builtins.h"
 #include "env.h"
 #include "utils.h"
+#include <err.h>
 #include <sys/wait.h>
 
 #define MAX_TMP_VARS 40
@@ -13,6 +14,7 @@ static int getbuiltin(char *);
 static int builtin_launch(int, char **);
 static int run_subsh(const cmd_tree *);
 static int run_cmd(const cmd_tree *);
+static int run_pipe(const cmd_tree *);
 static int shexec(char **, char **);
 
 typedef struct {
@@ -69,6 +71,49 @@ builtin_launch(int idx, char **args)
 
   fprintf(stderr, "simpsh: builtins you failed me!");
   return 1;
+}
+
+/** fork and exec external command */
+int
+shexec(char **args, char **env)
+{
+  int wstatus, estatus;
+  pid_t pid;
+  char *fullpath;
+
+  /* test if the command has a /, if not return the first executable
+     on PATH with the command name given */
+  fullpath = getpath(&args[0]);
+  if (!fullpath) {
+    fprintf(stderr, "%s: %s: command not found\n", sh_argv0, args[0]);
+    goto done;
+  }
+
+  pid = fork();
+  switch (pid) {
+  case -1:
+    perror("failed to create");
+    goto done;
+  case 0:
+    /* if fork was successful run the command */
+    if (execve(fullpath, args, env) == -1) {
+      perror(args[0]);
+      goto done;
+    }
+    /* fall through */
+  default:
+    waitpid(-1, &wstatus, 0);
+    estatus = WIFEXITED(wstatus) ? WEXITSTATUS(wstatus) : 1;
+    free(fullpath);
+    return estatus;
+  }
+  goto done;
+
+done:
+  if (fullpath)
+    free(fullpath);
+  estatus = 1;
+  return estatus;
 }
 
 int
@@ -152,13 +197,13 @@ run_cmd(const cmd_tree *n)
 int
 run_subsh(const cmd_tree *n)
 {
-  int status;
+  int status, wstatus;
+  pid_t pid;
+
   if (!n->left) {
     fprintf(stderr, "empty subshell\n");
     exit(1);
   }
-  int wstatus;
-  pid_t pid;
 
   pid = fork();
   switch (pid) {
@@ -177,47 +222,60 @@ run_subsh(const cmd_tree *n)
   }
 }
 
-/** fork and exec external command */
 int
-shexec(char **args, char **env)
+run_pipe(const cmd_tree *n)
 {
-  int wstatus, estatus;
-  pid_t pid;
-  char *fullpath;
+  int status, lwstatus, rwstatus, lstatus, rstatus;
+  int pipefd[2];
+  pid_t lpid, rpid;
 
-  /* test if the command has a /, if not return the first executable
-     on PATH with the command name given */
-  fullpath = getpath(&args[0]);
-  if (!fullpath) {
-    fprintf(stderr, "%s: %s: command not found\n", sh_argv0, args[0]);
-    goto done;
-  }
-
-  pid = fork();
-  switch (pid) {
+  if (pipe(pipefd) < 0)
+    err(1, "pipe");
+  lpid = fork();
+  switch (lpid) {
   case -1:
-    perror("failed to create");
-    goto done;
+    close(pipefd[0]);
+    close(pipefd[1]);
+    err(1, "fork");
   case 0:
-    /* if fork was successful run the command */
-    if (execve(fullpath, args, env) == -1) {
-      perror(args[0]);
-      goto done;
-    }
-    /* fall through */
-  default:
-    waitpid(-1, &wstatus, 0);
-    estatus = WIFEXITED(wstatus) ? WEXITSTATUS(wstatus) : 1;
-    free(fullpath);
-    return estatus;
+    if (close(pipefd[0]) < 0)
+      err(1, "close");
+    if (dup2(pipefd[1], STDOUT_FILENO) < 0)
+      err(1, "dup2");
+    if (close(pipefd[1]) < 0)
+      err(1, "close");
+    lstatus = run_commands(n->left);
+    _exit(lstatus);
   }
-  goto done;
 
-done:
-  if (fullpath)
-    free(fullpath);
-  estatus = 1;
-  return estatus;
+  rpid = fork();
+  switch (rpid) {
+  case -1:
+    close(pipefd[0]);
+    close(pipefd[1]);
+    kill(lpid, SIGTERM);
+    waitpid(lpid, NULL, 0);
+    err(1, "fork");
+  case 0:
+    if (close(pipefd[1]) < 0)
+      err(1, "close");
+    if (dup2(pipefd[0], STDIN_FILENO) < 0)
+      err(1, "dup2");
+    if (close(pipefd[0]) < 0)
+      err(1, "close");
+    rstatus = run_commands(n->right);
+    _exit(rstatus);
+  }
+
+  if (close(pipefd[0]) < 0)
+    err(1, "close");
+  if (close(pipefd[1]) < 0)
+    err(1, "close");
+  waitpid(lpid, &lwstatus, 0);
+  waitpid(rpid, &rwstatus, 0);
+  status = WIFEXITED(rwstatus) ? WEXITSTATUS(rwstatus) : 1;
+
+  return status;
 }
 
 /**  run command tree  */
@@ -226,31 +284,29 @@ run_commands(const cmd_tree *n)
 {
   if (!n)
     return 0;
-  int l_status, r_status, status;
+  int l_status = 0;
 
   switch (n->type) {
   case CMD:
-    status = run_cmd(n);
-    return status;
+    return run_cmd(n);
   case SUBSHELL:
-    status = run_subsh(n);
-    return status;
+    return run_subsh(n);
   case OP:
-    l_status = run_commands(n->left);
+    if (n->op_t != TPIPE)
+      l_status = run_commands(n->left);
     switch (n->op_t) {
     case TSEMI:
-      r_status = run_commands(n->right);
-      return r_status;
+      return run_commands(n->right);
     case TAND:
       if (l_status != 0)
         return l_status;
-      r_status = run_commands(n->right);
-      return r_status;
+      return run_commands(n->right);
     case TOR:
       if (l_status == 0)
         return l_status;
-      r_status = run_commands(n->right);
-      return r_status;
+      return run_commands(n->right);
+    case TPIPE:
+        return run_pipe(n);
     case TEOF:
       return l_status;
     default:
