2172410a7cc8337d44c7cdf967a67e6ecfacbbf9
blankyblank <jwthaparc@gmail.com>
2026-05-16 14:13:21 -0500

    just added a few lines

--- tests/08-alias.sh
+++ tests/08-alias.sh
@@ -6,9 +6,9 @@
 [ -f ./funcs ] && . ./funcs || { echo "no ./funcs file"; exit 1; }
 
 msg "running printf %s\n alias ec='echo test123' 'unalias ec' 'ec' | ../simpsh..."
-printf '%s\n' "alias ec='echo test123'" "unalias ec" "ec" | ../simpsh 2>/dev/null && { msg_fail "command should have failed" && exit 1;}
+printf '%s\n' "alias ec='echo test123'" "unalias ec" "ec" | ../simpsh  && { msg_fail "command should have failed" && exit 1;}
 
-msg "running out1=$(printf '%s\n' "alias ec='echo test123'" "ec" | ../simpsh)..."
+msg "running out1=\$(printf '%s\n' \"alias ec='echo test123'\" \"ec\" | ../simpsh)..."
 out1="$(printf '%s\n%s' "alias ec='echo test123'" "ec" | ../simpsh)"
 
 echo $out1
