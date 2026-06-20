d17d947fe192e58745ba5ef19d7340e798d8353f
blankyblank <jwthaparc@gmail.com>
2026-06-20 17:24:48 -0500

    added for loop test

--- tests/18-for.sh
+++ tests/18-for.sh
@@ -5,8 +5,9 @@
 
 [ -f ./for1.txt ] && rm ./for1.txt
 [ -f ./for2.txt ] && rm ./for2.txt
+[ -f ./for3.txt ] && rm ./for3.txt
 
-msg_run 'for loop test1: for f in *; do echo $f >> ./for1.txt; done'
+msg_run 'for loop test 1: for f in *; do echo $f >> ./for1.txt; done'
 ../simpsh -c 'for f in *; do echo $f >> ./for1.txt; done'
 out1=$(cat for1.txt | grep 18-for.sh)
 if [ "$out1" = "18-for.sh" ]; then
@@ -17,7 +18,7 @@ else
   exit 1
 fi
 
-msg_run 'for loop test1: for f in a b c; do echo $f >> ./for2.txt; done'
+msg_run 'for loop test 2: for f in a b c; do echo $f >> ./for2.txt; done'
 ../simpsh -c 'for f in a b c; do echo $f >> ./for2.txt; done'
 out2=$(tr -d '\n' <  for2.txt)
 if [ "$out2" = "abc" ]; then
@@ -27,3 +28,15 @@ else
   test_fail "out2" "expected" "abc"
   exit 1
 fi
+
+msg_run 'for loop test 3: set -- a b c; for f; do echo $f >> ./for3.txt; done'
+../simpsh -c 'set -- a b c; for f; do echo $f >> ./for3.txt; done'
+out3=$(tr -d '\n' <  for3.txt)
+if [ "$out3" = "abc" ]; then
+  test_pass "out3" "matches" "abc"
+  rm ./for3.txt
+else
+  test_fail "out3" "expected" "abc"
+  exit 1
+fi
+
